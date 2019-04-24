5c7971aca7eb70a66bc9316702cc9f3b253e47ec
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-23 23:52:05 -0300

    Update gradle plugin dependencies
    Gradle plugin to 3.4.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,22 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     @Suppress("RemoveRedundantBackticks")
     `kotlin-dsl`
+    kotlin("jvm") version "1.3.30"
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+    mavenCentral()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
