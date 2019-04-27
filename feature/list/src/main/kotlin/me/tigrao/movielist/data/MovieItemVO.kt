8c9d354e6589267dc42848098cfe243f2b0a9221
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-27 19:02:14 -0300

    Create VO for movies List

---
+++ feature/list/src/main/kotlin/me/tigrao/movielist/data/MovieItemVO.kt
@@ -0,0 +1,9 @@
+package me.tigrao.movielist.data
+
+internal data class MovieItemVO(
+    val title: String,
+    val overview: String,
+    val releaseDate: String,
+    val posterPath: String,
+    val genre: List<String>
+)
