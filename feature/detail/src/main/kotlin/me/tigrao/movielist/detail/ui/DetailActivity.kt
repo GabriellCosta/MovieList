045120e8fe4d41d53461127b99421ec5edf63273
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-28 20:38:52 -0300

    Add detail movie description

--- feature/detail/src/main/kotlin/me/tigrao/movielist/detail/ui/DetailActivity.kt
+++ feature/detail/src/main/kotlin/me/tigrao/movielist/detail/ui/DetailActivity.kt
@@ -3,7 +3,12 @@ package me.tigrao.movielist.detail.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import me.tigrao.aegis.commons.ui.bind
 import me.tigrao.movielist.data.MovieItemVO
 import me.tigrao.movielist.detail.R
 import org.kodein.di.Kodein
@@ -13,6 +18,12 @@ import org.kodein.di.android.closestKodein
 class DetailActivity : AppCompatActivity(), KodeinAware {
 
     override val kodein: Kodein by closestKodein()
+
+    private val posterView by bind<ImageView>(R.id.image_list_movie)
+    private val titleView by bind<TextView>(R.id.txt_list_movie_title)
+    private val overviewView by bind<TextView>(R.id.txt_list_movie_overview)
+    private val releaseDateView by bind<TextView>(R.id.txt_list_movie_release_date)
+    private val genresView by bind<TextView>(R.id.txt_list_moive_genre)
 
     companion object {
 
@@ -29,6 +40,25 @@ class DetailActivity : AppCompatActivity(), KodeinAware {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_detail)
+
+        prepareView()
     }
 
+    private fun prepareView() {
+        val vo = intent.extras!![EXTRA_DETAIL_ITEM] as MovieItemVO
+
+        bind(vo)
+    }
+
+    private fun bind(item: MovieItemVO) {
+        titleView.text = item.title
+        overviewView.text = item.overview
+        genresView.text = item.genre
+        releaseDateView.text = item.releaseDate
+
+        Glide.with(posterView)
+            .load("https://image.tmdb.org/t/p/w185/${item.posterPath}")
+            .apply(RequestOptions.circleCropTransform())
+            .into(posterView)
+    }
 }
