1bc36d74b71eb2b7801491f530511b508efea444
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-28 21:43:31 -0300

    Create database for movies

---
+++ feature/persistence/src/main/kotlin/me/tigrao/persistence/MovieDatabase.kt
@@ -0,0 +1,8 @@
+package me.tigrao.persistence
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import me.tigrao.movielist.data.MovieItemVO
+
+@Database(entities = [MovieItemVO::class], version = 1, exportSchema = false)
+abstract class MovieDatabase : RoomDatabase()
