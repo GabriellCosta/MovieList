770fd0282f662393b03466f43d40f74b9e236e3b
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-30 00:22:45 -0300

    Create tests for MoviListTransform

---
+++ feature/list/src/test/kotlin/me/tigrao/movielist/api/MovieListTransformTest.kt
@@ -0,0 +1,77 @@
+package me.tigrao.movielist.api
+
+import me.tigrao.movielist.data.GenreItemDTO
+import me.tigrao.movielist.data.GenreResponseDTO
+import me.tigrao.movielist.data.MovieItemDTO
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MovieListTransformTest {
+
+    private val movieListTransform = MovieListTransform(GenreMapTransform(createGenreDTOMock()))
+
+    fun createGenreDTOMock(): GenreResponseDTO {
+        return GenreResponseDTO(
+            listOf(
+                GenreItemDTO(
+                    1,
+                    "Action"
+                )
+            )
+        )
+    }
+
+    fun createMovieResponseMock() =
+        MovieItemDTO(
+            title = "Movie",
+            overview = "Overview",
+            releaseDate = "2019-04-04",
+            posterPath = "posterPath",
+            genre = listOf(1)
+        )
+
+    @Test
+    fun givenAMovieItem_withTitle_MovieVOShouldBeTheSame() {
+        val movieDTO = createMovieResponseMock()
+
+        val result = movieListTransform.map(movieDTO)
+
+        assertEquals(movieDTO.title, result.title)
+    }
+
+    @Test
+    fun givenMovieItem_withOverview_MovieVOShouldBetheSame() {
+        val movieDTO = createMovieResponseMock()
+
+        val result = movieListTransform.map(movieDTO)
+
+        assertEquals(movieDTO.overview, result.overview)
+    }
+
+    @Test
+    fun givenMovieItem_withPosterPath_MovieVOShouldBetheSame() {
+        val movieDTO = createMovieResponseMock()
+
+        val result = movieListTransform.map(movieDTO)
+
+        assertEquals(movieDTO.posterPath, result.posterPath)
+    }
+
+    @Test
+    fun givenMovieItem_withReleaseDate_MovieVOShouldBetheSame() {
+        val movieDTO = createMovieResponseMock()
+
+        val result = movieListTransform.map(movieDTO)
+
+        assertEquals(movieDTO.releaseDate, result.releaseDate)
+    }
+
+    @Test
+    fun givenMovieItem_withGenreList_MovieVOShouldHaveConvertedGenreList() {
+        val movieDTO = createMovieResponseMock()
+
+        val result = movieListTransform.map(movieDTO)
+
+        assertEquals(listOf("Action").toString(), result.genre)
+    }
+}
